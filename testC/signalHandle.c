db8a484e13b9ada3fdd589a87db6f8b9c8303908
MiyuruCG <miyurugunatilake@gmail.com>
2020-05-09 17:42:32 +0530

    signal handler test

---
+++ testC/signalHandle.c
@@ -0,0 +1,23 @@
+#include <signal.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void sig_handler(int signum)
+{
+    printf("Alarm delivered signnum is %d\n", signum);
+    exit(0);
+}
+
+int main()
+{
+    alarm(5);
+    signal(SIGALRM, sig_handler);
+
+    for (;;)
+    {
+        sleep(1);
+    }
+
+    return 0;
+}
